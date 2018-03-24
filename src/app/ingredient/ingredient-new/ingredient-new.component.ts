2104a94ab2c6515d9687ae0423009e2abdd5f872
Spencer Larsen <larsen.spener@gmail.com>
2018-03-24 11:36:23 -0700

    File upload work

--- src/app/ingredient/ingredient-new/ingredient-new.component.ts
+++ src/app/ingredient/ingredient-new/ingredient-new.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, NgModule } from '@angular/core';
+import { Component, Output, EventEmitter, NgModule, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -17,6 +17,16 @@ export class IngredientNewComponent {
   ingredient : Ingredient = new Ingredient();
 
   ingredientErrors: string[] = [];
+
+  acceptedMimeTypes = [
+    'image/gif',
+    'image/jpeg',
+    'image/png',
+  ];
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+  fileDataUri = '';
+  errorMsg = '';
 
   @Output()
   addIngredient = new EventEmitter<Ingredient>();
@@ -41,11 +51,39 @@ export class IngredientNewComponent {
       console.log(`We submitted an ingredient`);
     }
 
+  previewFile() : void {
+    const file = this.fileInput.nativeElement.files[0];
+    if(file && this.validateFile(file)){
+      const reader = new FileReader();
+      console.log(this.fileInput.nativeElement.files)
+      reader.readAsDataURL(this.fileInput.nativeElement.files[0]);
+      reader.onload = () => {
+        this.fileDataUri = reader.result;
+      }
+    } else {
+      this.errorMsg = 'File must be a jpg, png, or gif and cannot exceed 500 KB in size';
+    }
+  }
+
+  uploadFile(event : Event) : void {
+    event.preventDefault();
+
+    if(this.fileDataUri.length > 0){
+      console.log(this.fileDataUri)
+      const base64File = this.fileDataUri.split(',')[1];
+      console.log(base64File);
+    }
+  }
+
   logout(event: Event): void {
     event.stopPropagation();
     this.auth.logout()
       .then(() => this.router.navigate(['home']))
       .catch(response => console.log(response.json()));
+  }
+
+  private validateFile(file): boolean {
+    return this.acceptedMimeTypes.includes(file.type) && file.size < 5000000;
   }
 
   private handleErrors(errors: string[] | Error): void {
